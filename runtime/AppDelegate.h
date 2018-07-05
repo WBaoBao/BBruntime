c5ba80725d73316caafa6ecacd74dd278c112184
WangBaoMing <1250829271@qq.com>
2018-07-05 16:19:03 +0800

    Initial Commit

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by apple on 2018/7/5.
+//  Copyright © 2018年 baobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
